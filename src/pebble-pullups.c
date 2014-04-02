3051376a7aebce7f3fa658a9f28cbb23df76301f
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2014-04-02 20:19:25 +0400

    pullups: add simple timer mode
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

--- src/pebble-pullups.c
+++ src/pebble-pullups.c
@@ -1,14 +1,41 @@
 #include <pebble.h>
 
+#define STATE_PAUSE 0
+#define STATE_PLAY 1
+
+#define DEFAULT_TIME 30
+
+int cur_time = DEFAULT_TIME;
+int cur_reps = 1;
+
 static Window *main_win;
 static ActionBarLayer *buttons;
 static Layer *main_layer;
-static GBitmap *play_button;
+static GBitmap *play_button, *pause_button;
+static int state = STATE_PAUSE;
 char str_reps[16], str_time[16];
 
 static void pullups_tick_handler(struct tm *time, TimeUnits unit);
 static unsigned pullups_get_current_reps(void);
 static unsigned pullups_get_current_time(void);
+
+static void pullups_set_state()
+{
+	if (state == STATE_PAUSE) {
+		if (cur_time == DEFAULT_TIME) cur_reps++;
+		tick_timer_service_subscribe(SECOND_UNIT, pullups_tick_handler);
+		action_bar_layer_set_icon(buttons, BUTTON_ID_DOWN, pause_button);
+	} else {
+		tick_timer_service_unsubscribe();
+		action_bar_layer_set_icon(buttons, BUTTON_ID_DOWN, play_button);
+	}
+	state = !state;
+}
+
+static void pullups_play_pause_handler(ClickRecognizerRef rec, void *context)
+{
+	pullups_set_state();
+}
 
 static void pullups_main_layer_update(Layer *layer, GContext *ctx)
 {
@@ -36,16 +63,24 @@ static void pullups_main_layer_update(Layer *layer, GContext *ctx)
 		GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);
 }
 
+static void click_config_provider(void *context)
+{
+	window_single_click_subscribe(BUTTON_ID_DOWN,
+			(ClickHandler)pullups_play_pause_handler);
+}
 static void pullups_action_bar_add(Window *win)
 {
 	play_button = gbitmap_create_with_resource(RESOURCE_ID_PLAY_BUTTON);
+	pause_button = gbitmap_create_with_resource(RESOURCE_ID_PAUSE_BUTTON);
 	buttons = action_bar_layer_create();
 	action_bar_layer_add_to_window(buttons, win);
+	action_bar_layer_set_click_config_provider(buttons, click_config_provider);
 	action_bar_layer_set_icon(buttons, BUTTON_ID_DOWN, play_button);
 }
 static void pullups_action_bar_del(Window *win)
 {
 	action_bar_layer_destroy(buttons);
+	gbitmap_destroy(pause_button);
 	gbitmap_destroy(play_button);
 }
 
@@ -100,17 +135,44 @@ static void deinit(void)
 	window_destroy(main_win);
 }
 
+static void pullups_vibe(void)
+{
+	switch(cur_time) {
+		case 10: case 3:  case 2: case 1:
+			vibes_short_pulse(); break;
+		case 0:
+			vibes_long_pulse(); break;
+		default:
+			break;
+	}
+}
+
+static void pullups_one_tick()
+{
+	cur_time--;
+	pullups_vibe();
+	if (cur_time)
+		return;
+	cur_time = DEFAULT_TIME;
+	pullups_set_state(STATE_PAUSE);
+}
+
 static void pullups_tick_handler(struct tm *time, TimeUnits unit)
 {
+	if (unit != SECOND_UNIT)
+		return;
+
+	pullups_one_tick();
+	layer_mark_dirty(main_layer);
 }
 
 static unsigned pullups_get_current_reps(void)
 {
-	return 0;
+	return cur_reps;
 }
 static unsigned pullups_get_current_time(void)
 {
-	return 0;
+	return cur_time;
 }
 
 int main(void)
