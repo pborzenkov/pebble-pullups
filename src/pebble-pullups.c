c86669c566172a5a8d80a8250a7a38514b6bccd4
Pavel Borzenkov <pavel.borzenkov@gmail.com>
2014-04-02 23:13:10 +0400

    pullups: change icon and forbid to pause
    Change icon to represent correct action: 'check' that pull-ups are done.
    Also, forbid pause since the action is post-factum.
    
    Signed-off-by: Pavel Borzenkov <pavel.borzenkov@gmail.com>

--- src/pebble-pullups.c
+++ src/pebble-pullups.c
@@ -1,8 +1,5 @@
 #include <pebble.h>
 
-#define STATE_PAUSE 0
-#define STATE_PLAY 1
-
 #define DEFAULT_TIME 30
 
 int cur_time = DEFAULT_TIME;
@@ -11,30 +8,21 @@ int cur_reps = 1;
 static Window *main_win;
 static ActionBarLayer *buttons;
 static Layer *main_layer;
-static GBitmap *play_button, *pause_button;
-static int state = STATE_PAUSE;
+static GBitmap *check_button;
 char str_reps[16], str_time[16];
+static int ticking;
 
 static void pullups_tick_handler(struct tm *time, TimeUnits unit);
 static unsigned pullups_get_current_reps(void);
 static unsigned pullups_get_current_time(void);
 
-static void pullups_set_state()
-{
-	if (state == STATE_PAUSE) {
-		if (cur_time == DEFAULT_TIME) cur_reps++;
-		tick_timer_service_subscribe(SECOND_UNIT, pullups_tick_handler);
-		action_bar_layer_set_icon(buttons, BUTTON_ID_DOWN, pause_button);
-	} else {
-		tick_timer_service_unsubscribe();
-		action_bar_layer_set_icon(buttons, BUTTON_ID_DOWN, play_button);
-	}
-	state = !state;
-}
-
 static void pullups_play_pause_handler(ClickRecognizerRef rec, void *context)
 {
-	pullups_set_state();
+	if (ticking)
+		return;
+	ticking = 1;
+	cur_reps++;
+	tick_timer_service_subscribe(SECOND_UNIT, pullups_tick_handler);
 }
 
 static void pullups_main_layer_update(Layer *layer, GContext *ctx)
@@ -44,7 +32,7 @@ static void pullups_main_layer_update(Layer *layer, GContext *ctx)
 	snprintf(str_reps, sizeof(str_reps),
 			"reps: %u\n", pullups_get_current_reps());
 	snprintf(str_time, sizeof(str_time),
-			"%02u:%02u\n", time / 60, time % 60);
+			"%0u:%02u\n", time / 60, time % 60);
 
 	graphics_context_set_text_color(ctx, GColorBlack);
 	graphics_draw_text(ctx, str_reps,
@@ -70,18 +58,16 @@ static void click_config_provider(void *context)
 }
 static void pullups_action_bar_add(Window *win)
 {
-	play_button = gbitmap_create_with_resource(RESOURCE_ID_PLAY_BUTTON);
-	pause_button = gbitmap_create_with_resource(RESOURCE_ID_PAUSE_BUTTON);
+	check_button = gbitmap_create_with_resource(RESOURCE_ID_CHECK_BUTTON);
 	buttons = action_bar_layer_create();
 	action_bar_layer_add_to_window(buttons, win);
 	action_bar_layer_set_click_config_provider(buttons, click_config_provider);
-	action_bar_layer_set_icon(buttons, BUTTON_ID_DOWN, play_button);
+	action_bar_layer_set_icon(buttons, BUTTON_ID_DOWN, check_button);
 }
 static void pullups_action_bar_del(Window *win)
 {
 	action_bar_layer_destroy(buttons);
-	gbitmap_destroy(pause_button);
-	gbitmap_destroy(play_button);
+	gbitmap_destroy(check_button);
 }
 
 static void pullups_main_layer_add(Window *win)
@@ -137,7 +123,7 @@ static void deinit(void)
 
 static void pullups_vibe(void)
 {
-	switch(cur_time) {
+	switch(pullups_get_current_time()) {
 		case 10: case 3:  case 2: case 1:
 			vibes_short_pulse(); break;
 		case 0:
@@ -153,8 +139,9 @@ static void pullups_one_tick()
 	pullups_vibe();
 	if (cur_time)
 		return;
+	tick_timer_service_unsubscribe();
 	cur_time = DEFAULT_TIME;
-	pullups_set_state(STATE_PAUSE);
+	ticking = 0;
 }
 
 static void pullups_tick_handler(struct tm *time, TimeUnits unit)
